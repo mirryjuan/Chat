690baa18fff8298d5d2b2f4d3de811b0a7d15109
mirryjuan <1599270722@qq.com>
2017-02-14 12:26:45 +0800

    第一次提交H5代码

--- app/src/main/java/com/example/mirry/chat/fragment/AppsFragment.java
+++ app/src/main/java/com/example/mirry/chat/fragment/AppsFragment.java
@@ -5,11 +5,27 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.mirry.chat.R;
 import com.example.mirry.chat.activity.MainActivity;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
 public class AppsFragment extends Fragment {
+    @InjectView(R.id.scan)
+    TextView scan;
+    @InjectView(R.id.robot)
+    TextView robot;
+    @InjectView(R.id.record)
+    TextView record;
+    @InjectView(R.id.news)
+    TextView news;
+    @InjectView(R.id.weather)
+    TextView weather;
+    @InjectView(R.id.share)
+    TextView share;
     private MainActivity mActivity;
 
     @Override
@@ -21,9 +37,15 @@ public class AppsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_apps,container,false);
+        View view = inflater.inflate(R.layout.fragment_apps, container, false);
 
+        ButterKnife.inject(this, view);
         return view;
     }
 
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.reset(this);
+    }
 }
