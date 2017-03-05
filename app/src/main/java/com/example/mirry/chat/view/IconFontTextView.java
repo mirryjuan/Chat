e624aedd4f04e653c53e6918874291968825722b
mirryjuan <1599270722@qq.com>
2017-03-05 12:12:50 +0800

    消息界面

---
+++ app/src/main/java/com/example/mirry/chat/view/IconFontTextView.java
@@ -0,0 +1,31 @@
+package com.example.mirry.chat.view;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by Mirry on 2017/3/5.
+ */
+
+public class IconFontTextView extends TextView {
+    public  IconFontTextView(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public IconFontTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public IconFontTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+    private void init(Context context){
+        Typeface iconfont = Typeface.createFromAsset(context.getAssets(), "iconFonts/iconfont.ttf");
+        setTypeface(iconfont);
+    }
+}
