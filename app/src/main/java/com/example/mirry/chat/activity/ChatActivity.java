3b619d89fc489a3630ea98eb364270eacc855eed
mirryjuan <1599270722@qq.com>
2017-03-05 12:14:49 +0800

    增加聊天界面

---
+++ app/src/main/java/com/example/mirry/chat/activity/ChatActivity.java
@@ -0,0 +1,18 @@
+package com.example.mirry.chat.activity;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+import com.example.mirry.chat.R;
+
+public class ChatActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_chat);
+    }
+}
